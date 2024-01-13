e9836c399fe20af5dabc62e71f601722fe3b02ae
Augusto Ramon Favre <augustofavrearg@gmail.com>
2024-01-13 01:40:01 -0300

    Prueba tailwind para login y paidPlans. Conectar front + back.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from './views/Login';
+import PaidPlans from './views/PaidPlans';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Routes>
       <Route path='/login' element={<Login />}></Route>
+      <Route path='/paidPlans' element={<PaidPlans />}></Route>
       </Routes>
     </>
   )
---
+++ client/src/views/PaidPlans.jsx
@@ -0,0 +1,12 @@
+
+import PaidPlansComponent from '../components/PaidPlansComponent'
+
+const PaidPlans = () => {
+  return (
+    <div>
+      <PaidPlansComponent />
+    </div>
+  )
+}
+
+export default PaidPlans
