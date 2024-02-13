73491ff4c85cae433dd9c5a1ada41c9dc62054bd
Ramiro <ramiro.alet@gmail.com>
2024-02-13 01:17:52 -0300

    env

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // Carga las variables de entorno según el modo de ejecución (development, production, etc.)
+  dotenv.config({ path: `./.env.${mode}` });
+
+  return {
+    plugins: [
+      react()
+    ],
+    // Otras configuraciones de Vite...
+  };
+});
