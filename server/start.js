310c7d646c594d7daa309c152dec17598bdbac9a
Apptrader <proyectoapptrader@gmail.com>
2024-03-21 01:46:27 -0300

    sss

--- server/start.js
+++
@@ -1,7 +0,0 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-
-const esmConfig = await import('./ecosystem.config.mjs');
-const { default: appsConfig } = esmConfig;
-
-module.exports = appsConfig;
