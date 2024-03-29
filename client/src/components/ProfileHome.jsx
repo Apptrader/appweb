9b090f010e8c46d3f0410f1ec6056d9fc6580b6f
Apptrader <proyectoapptrader@gmail.com>
2024-03-29 16:36:10 -0300

    final por ahora

--- client/src/components/ProfileHome.jsx
+++ client/src/components/ProfileHome.jsx
@@ -5,6 +5,7 @@ import ProfileDetail from "./ProfileDetail";
 import ReferralTree from "./ReferralTree";
 import axios from "axios";
 import getParamsEnv from "../functions/getParamsEnv";
+import { useMediaQuery } from "@react-hook/media-query"; // Importa useMediaQuery
 
 const {API_URL_BASE} = getParamsEnv()
 
@@ -16,11 +17,7 @@ const ProfileHome = ({user}) => {
   })
 
   const [isLoading, setIsLoading] = useState(true)
-
- 
-
-
-  const userInfo = user.userFound
+  const userInfo = user.userFound;
   const [referred, setReferred] = useState({})
 
   const ranks = {
@@ -28,66 +25,61 @@ const ProfileHome = ({user}) => {
     highestRankId: userInfo.highestRank
   }
 
-
   useEffect(() => {
     const getRankNames = async () => {
-
-      const results = await axios.post(`${API_URL_BASE}/api/rank/getName`, ranks )
+      const results = await axios.post(`${API_URL_BASE}/api/rank/getName`, ranks)
       setRankNames(results.data)
     }
 
     const getReferralInfo = async () => {
-     
-        const result = await axios.post(`${API_URL_BASE}/apiUser/getUserByUserCode`, {userCode: userInfo.CodeReferenced})
-        setReferred(result.data)
-    
-      
+      const result = await axios.post(`${API_URL_BASE}/apiUser/getUserByUserCode`, {userCode: userInfo.CodeReferenced})
+      setReferred(result.data)
     }
 
-    
-
     if(userInfo.CodeReferenced !== "")  {
-    getReferralInfo()
+      getReferralInfo()
     }
 
     getRankNames()
     setIsLoading(false)
   },[])
 
-
   const calculate = async () => {
-   
     const response = await axios.post(`${API_URL_BASE}/apiUser/calculate`)
-  
   }
 
+  const isMobile = useMediaQuery("(max-width: 768px)"); // Define si es móvil
 
-if (!isLoading) {
-  return (
-    <div className='container m-auto w-full px-10'>
-      <div className='flex flex-row gap-5'>
-        <div className=' bg-gray-800 w-3/5' style={{ flex: '1', padding: '20px' }}>
-         
-          <ReferralTree />
-          
-        </div>
-        <div className='bg-gray-900 w-1/5' style={{ flex: '1', padding: '20px' }}>
-         
-          <ProfileCard userInfo={userInfo} rankNames={rankNames} referred={referred}  />
-        </div>
-        <div className='bg-gray-900 w-1/5' style={{ flex: '1', padding: '20px' }}>
-          {/* Reduje el ancho del tercer componente a w-1/5 */}
-          <ProfileDetail userInfo={userInfo} rankNames={rankNames} token={user.token} />
-        </div>
+  if (!isLoading) {
+    return (
+      <div className='container m-auto w-full px-10'>
+        {/* Cambio de estructura dependiendo del tamaño de la pantalla */}
+        {isMobile ? (
+          <>
+            <ReferralTree />
+            <ProfileCard userInfo={userInfo} rankNames={rankNames} referred={referred} />
+            <ProfileDetail userInfo={userInfo} rankNames={rankNames} token={user.token} />
+          </>
+        ) : (
+          <div className='flex flex-row gap-5'>
+            <div className='bg-gray-800 w-3/5' style={{ flex: '1', padding: '20px' }}>
+              <ReferralTree />
+            </div>
+            <div className='bg-gray-900 w-1/5' style={{ flex: '1', padding: '20px' }}>
+              <ProfileCard userInfo={userInfo} rankNames={rankNames} referred={referred} />
+            </div>
+            <div className='bg-gray-900 w-1/5' style={{ flex: '1', padding: '20px' }}>
+              <ProfileDetail userInfo={userInfo} rankNames={rankNames} token={user.token} />
+            </div>
+          </div>
+        )}
       </div>
-    </div>
-  )
-} else {
-  return (
-    <p>Cargando</p>
-  )
-}
-  
-}
+    );
+  } else {
+    return (
+      <p>Cargando</p>
+    );
+  }
+};
 
 export default ProfileHome;
--- client/src/views/VideoPanelView.jsx
+++ client/src/views/VideoPanelView.jsx
@@ -1,30 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import NavbarComponent from '../components/NavbarComponent'
-import SidebarComponent from '../components/SidebarComponent'
-import { useSelector } from 'react-redux';
+import React from 'react';
+import NavbarComponent from '../components/NavbarComponent';
+import SidebarComponent from '../components/SidebarComponent';
 import VideosTable from '../components/VideosTable';
-
-
-
-
-
+import { useSelector } from 'react-redux';
+import { useMediaQuery } from 'react-responsive';
 
 const UsersView = () => {
-
   const user = useSelector((state) => state?.user);
+  const isDesktopOrLaptop = useMediaQuery({ minDeviceWidth: 1224 });
 
-  // Verifica si user y user.userFound no son null o undefined antes de acceder a la propiedad role
   const role = user?.userFound?.role;
-   return (
-        <div >
-    
-          <NavbarComponent />
-          <div className='flex flex-row bg-black'>
-          {role === "1" && <SidebarComponent className="flex-1" />}
+
+  return (
+    <div>
+      <NavbarComponent />
+      <div className='flex flex-col bg-black'>
+        {isDesktopOrLaptop ? (
+          <div className='flex flex-row'>
+            {role === "1" && <SidebarComponent />}
             <VideosTable />
           </div>
-        </div>
-      );
+        ) : (
+          <>
+            <div className='flex flex-row'>
+              <VideosTable />
+            </div>
+            {role === "1" && <SidebarComponent isMobile />}
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default UsersView
+export default UsersView;
