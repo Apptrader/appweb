15941f227eefcdf6737249373627b7f77f2fa02f
Apptrader <proyectoapptrader@gmail.com>
2024-04-11 16:21:09 -0300

    asdadsa

--- client/src/components/PaidPlansListComponent.jsx
+++ client/src/components/PaidPlansListComponent.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+/* import { useState, useEffect } from 'react';
 import axios from 'axios';
 import NavbarComponent from './NavbarComponent';
 import 'animate.css';
@@ -304,8 +304,8 @@ const PaidPlansListComponent = ({user}) => {
 }
 
 export default PaidPlansListComponent;
-
-/* import { useState, useEffect } from 'react';
+ */
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import NavbarComponent from './NavbarComponent';
 import 'animate.css';
@@ -603,4 +603,4 @@ const PaidPlansListComponent = ({user}) => {
   );
 }
 
-export default PaidPlansListComponent; */
+export default PaidPlansListComponent;
